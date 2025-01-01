3c784258744feb00a927247c27ed0b7f48e37214
Anton Voytov <august-ra@ya.ru>
2025-01-01 13:43:00 +0300

    Made an example with a template

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,6 +10,32 @@ def index(name: str = None):
         name = "World"
 
     return f"Hello, {name.capitalize()}!"
+
+
+@app.route("/matrix")
+def matrix():
+    return render_template("films.html", films=get_matrix_films())
+
+
+def get_matrix_films():
+    return [
+        {
+            "title": "The Matrix",
+            "year": 1999,
+        },
+        {
+            "title": "The Matrix Reloaded",
+            "year": 2003,
+        },
+        {
+            "title": "The Matrix Revolutions",
+            "year": 2003,
+        },
+        {
+            "title": "The Animatrix",
+            "year": 2003,
+        },
+    ]
 
 
 if __name__ == "__main__":
