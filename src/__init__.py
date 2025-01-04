deff2df490ae0f69025a7dc26c157b7075a71f29
Anton Voytov <august-ra@ya.ru>
2025-01-04 14:52:05 +0300

    Added SwaggerAPI and two specifications (yaml + json)

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,24 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from config import Config
 
+
+SWAGGER_URL = "/swagger"
+API_URL = "/static/swagger.yaml"
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        "app_name": "Flask Proba",
+    }
+)
+
 app = Flask(__name__)
 app.config.from_object(Config)
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
